dd7b5c58e31b7fedbf4bacf3a127ff0fbbacc025
PAUL M YARANGGA <paulyarangga07@gmail.com>
2022-03-19 11:47:29 +0900

    add index and student files

---
+++ students3.php
@@ -0,0 +1,7 @@
+<?php
+    $students = array(
+        array("Beri Pahabol", 19, "beri.pahabol@gmail.com"),
+        array("Marina Rumi", 17, "marinarumi@gmail.com"),
+        array("Yekson Bagau", 19, "yeksonganteng@gmail.com"),
+    );
+?>
